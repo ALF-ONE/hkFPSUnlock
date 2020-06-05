66481394731825f7fd0d84b5a9791770f1935163
ALF-ONE <ALF-ONE@users.noreply.github.com>
2020-06-05 15:00:45 +0300

    sleep patch
    deleted sleep(1) in samp hook

--- FPSUnlock/FPSUnlock.cpp
+++ FPSUnlock/FPSUnlock.cpp
@@ -18,16 +18,18 @@ public:
 					urmem::sig_scanner scan;
 					if (!scan.init(baseAddress)) break;
 
-					urmem::address_t AddClientMessageAddress, FPSUnlockAddress, FPSLimitAddress;
+					urmem::address_t AddClientMessageAddress, FPSUnlockAddress, FPSLimitAddress, SleepHookAddress;
 					if (!scan.find("\x56\x8B\x74\x24\x0C\x8B\xC6\x57\x8B\xF9", "xxxxxxxxxx", AddClientMessageAddress)) break;
 					if (!scan.find("\xE8\x00\x00\x00\x00\xA1\x00\x00\x00\x00\x85\xC0\x74\x00\x8B\x80\x00\x00\x00\x00\x8B\x40", "x????x????xxx?xx????xx", FPSUnlockAddress)) break;
 					if (!scan.find("\x83\xFE\x14\x72\x00\x83\xFE\x5A", "xxxx?xxx", FPSLimitAddress)) break;
+					if (!scan.find("\x6A\x01\xFF\x15\x00\x00\x00\x00\xBA\x80\x1A\x56\x00", "xxxx????xxxxx", SleepHookAddress)) break;
 
 					hkADDClientMessage = std::make_shared<urmem::hook>(AddClientMessageAddress, urmem::get_func_addr(&HOOK_ADDClientMessage), urmem::hook::type::jmp, 5);
 					hkFPSUnlock = std::make_shared<urmem::hook>(FPSUnlockAddress, urmem::get_func_addr(&HOOK_FPSUnlock), urmem::hook::type::call, 5);
 					FPSLimitPatch[0] = std::make_shared<urmem::patch>(FPSLimitAddress, urmem::bytearray_t{ 0x56 });
 					FPSLimitPatch[1] = std::make_shared<urmem::patch>(FPSLimitAddress + 0x2D, urmem::bytearray_t(20, 0x90));
 					hkFPSLimit = std::make_shared<urmem::hook>(FPSLimitAddress + 0x01, urmem::get_func_addr(&HOOK_FPSLimit), urmem::hook::type::call, 9);
+					FPSLimitPatch[2] = std::make_shared<urmem::patch>(SleepHookAddress, urmem::bytearray_t(8, 0x90));
 
 					gamePatch[0] = std::make_shared<urmem::patch>(0xBAB318, urmem::bytearray_t{ 0x00 });
 					gamePatch[1] = std::make_shared<urmem::patch>(0x53E94C, urmem::bytearray_t{ 0x00 });
@@ -74,13 +76,13 @@ private:
 
 	static void* _thisChat;
 	static std::shared_ptr<CThread> Thread;
-	static std::shared_ptr<urmem::patch> FPSLimitPatch[2], gamePatch[2];
+	static std::shared_ptr<urmem::patch> FPSLimitPatch[3], gamePatch[2];
 	static std::shared_ptr<urmem::hook> hkADDClientMessage, hkFPSUnlock, hkFPSLimit;
 };
 
 void* FPSUnlock::_thisChat;
 std::shared_ptr<FPSUnlock::CThread> FPSUnlock::Thread;
-std::shared_ptr<urmem::patch> FPSUnlock::FPSLimitPatch[2], FPSUnlock::gamePatch[2];
+std::shared_ptr<urmem::patch> FPSUnlock::FPSLimitPatch[3], FPSUnlock::gamePatch[2];
 std::shared_ptr<urmem::hook> FPSUnlock::hkADDClientMessage, FPSUnlock::hkFPSUnlock, FPSUnlock::hkFPSLimit;
 
 auto APIENTRY DllMain(HMODULE hModule, DWORD dwReasonForCall, LPVOID lpReserved) -> BOOL
